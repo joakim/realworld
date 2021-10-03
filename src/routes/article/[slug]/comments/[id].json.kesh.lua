5b87a5510016ba273465ba1718bfc2ff02642afd
Joakim <joakim@users.noreply.github.com>
2021-10-03 21:54:33 +0200

    Update and rename [id].json.js to [id].json.kesh.lua

---
+++ src/routes/article/[slug]/comments/[id].json.kesh.lua
@@ -0,0 +1,11 @@
+api: import '$lib/api.js'
+
+del: async ([ :params, :locals ]) ->
+	if not locals.user -> return [ status: 401 ]
+	
+	[:slug, :id]: params
+	[:status, :error]: await api.del("articles/{ slug }/comments/{ id }", locals.user.token)
+	
+	if error -> return [ :status, body: [ :error ] ]
+
+(del)
