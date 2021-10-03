82c0a9982eb0d387a426f04a85092f51b4ef98c0
Joakim <joakim@users.noreply.github.com>
2021-10-03 19:19:13 +0200

    Update and rename index.js to index.kesh.lua

---
+++ src/hooks/index.kesh.lua
@@ -0,0 +1,21 @@
+cookie: import 'cookie'
+
+handle: async ([ :request, :resolve ]) -> {
+	cookies: cookie.parse(request.headers.cookie ? '')
+	jwt: cookies.jwt and Buffer.from(cookies.jwt, 'base64').toString('utf-8')
+	set request.locals.user: JSON.parse(jwt) if jwt else null
+	await resolve request
+}
+
+get-session: ([ :locals ]) -> {
+	[
+		user: locals.user and {
+			username: locals.user.username
+			email: locals.user.email
+			image: locals.user.image
+			bio: locals.user.bio
+		]
+	]
+}
+
+(handle, get-session)
