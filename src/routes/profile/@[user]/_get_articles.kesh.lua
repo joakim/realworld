c46d3e7a67d47caf7e87f0aa966d87ef42e862d9
Joakim <joakim@users.noreply.github.com>
2021-10-03 20:08:10 +0200

    Update and rename _get_articles.js to _get_articles.kesh.lua

---
+++ src/routes/profile/@[user]/_get_articles.kesh.lua
@@ -0,0 +1,25 @@
+api: import '$lib/api.js'
+(page-size): '$lib/constants.js'
+
+get-articles: async ([ :query, params, locals ], type) -> {
+	p: +query.get('page') or 1
+
+	q: URLSearchParams [
+		['limit', page-size]
+		['offset', (p - 1) * page-size]
+		[type, encodeURIComponent params.user]
+	]
+
+	[ :articles, :articles-count ]: await api.get(
+		"articles?{ q }"
+		locals.user and locals.user.token
+	)
+
+	[
+		body:
+			:articles,
+			pages: Math.ceil(articles-count / page-size)
+	]
+}
+
+(get-articles)
