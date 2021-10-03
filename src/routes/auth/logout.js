406dfbfcd6dda0566592a35fecb6a1dc85c9a711
Joakim <joakim@users.noreply.github.com>
2021-10-03 20:50:36 +0200

    Update logout.js

--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -1,10 +1,8 @@
-export function post() {
-	return {
-		headers: {
-			'set-cookie': 'jwt=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
-		},
-		body: {
-			ok: true
-		}
-	};
-}
+(post: () -> [
+	headers:
+		'set-cookie': 'jwt=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
+	body:
+		ok: true
+])
+
+(post)
