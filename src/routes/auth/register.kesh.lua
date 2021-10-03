8d843556cfaa82ef13eb7d8888fce7122b315d4b
Joakim <joakim@users.noreply.github.com>
2021-10-03 20:52:01 +0200

    Update and rename register.js to register.kesh.lua

---
+++ src/routes/auth/register.kesh.lua
@@ -0,0 +1,13 @@
+api: '$lib/api.js'
+(respond): import './_respond'
+
+post: async (request) -> {
+	user: request.body
+	
+	-- TODO individual properties
+	body: await api.post('users', [ :user ])
+	
+	respond body
+}
+
+(post)
