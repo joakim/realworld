d72ac4716c84e615985264a7fbcc5f5312d0f0a3
Joakim <joakim@users.noreply.github.com>
2021-10-03 19:38:48 +0200

    Update articles.json.js

--- src/routes/articles.json.js
+++ src/routes/articles.json.js
@@ -1,31 +1,32 @@
-import * as api from '$lib/api';
-import { page_size } from '$lib/constants';
+api: import '$lib/api'
+(page_size): import '$lib/constants'
 
-export async function get({ query, locals }) {
-	const tab = query.get('tab') || 'all';
-	const tag = query.get('tag');
-	const page = +query.get('page') || 1;
-
-	const endpoint = tab === 'feed' ? 'articles/feed' : 'articles';
-
-	const q = new URLSearchParams();
-
-	q.set('limit', page_size);
-	q.set('offset', (page - 1) * page_size);
-
-	if (tag) {
-		q.set('tag', tag);
+get: async ([ :query, :locals ]) -> {
+	tab: query.get('tab') or 'all'
+	tag: query.get('tag')
+	page: +query.get('page') or 1
+	
+	endpoint: 'articles/feed' if tab = 'feed' else 'articles'
+	
+	q: URLSearchParams()
+	
+	q.set('limit', page_size)
+	q.set('offset', (page - 1) * page_size)
+	
+	if tag {
+		q.set('tag', tag)
 	}
-
-	const { articles, articlesCount } = await api.get(
-		`${endpoint}?${q}`,
-		locals.user && locals.user.token
-	);
-
-	return {
-		body: {
-			articles,
+	
+	[ :articles, :articlesCount ]: await api.get(
+		"{endpoint}?{q}"
+		locals.user and locals.user.token
+	)
+	
+	[
+		body:
+			:articles
 			pages: Math.ceil(articlesCount / page_size)
-		}
-	};
+	]
 }
+
+(get)
