a7a62281a995a1607c8411d9d619641039176348
Joakim <joakim@users.noreply.github.com>
2021-10-03 21:06:42 +0200

    Update and rename index.json.js to index.json.kesh.lua

---
+++ src/routes/article/[slug]/index.json.kesh.lua
@@ -0,0 +1,14 @@
+api: import '$lib/api.js'
+
+get: async ([ :params, :locals ]) -> {
+	[:slug]: params
+	[:article]: await api.get("articles/{ slug }", locals.user and locals.user.token)
+	
+	[ body: article ]
+}
+
+put: async (request) -> *{
+	print('put', request)
+}
+
+(get, put)
