41d4704488b635770fb884f66fcb502e40c814b9
Joakim <joakim@users.noreply.github.com>
2021-10-03 19:58:18 +0200

    Update and rename follow.js to follow.kesh.lua

---
+++ src/routes/profile/@[user]/follow.kesh.lua
@@ -0,0 +1,11 @@
+api: '$lib/api.js'
+
+post: async ([ :params, :locals ]) -> [
+	body: await api.post("profiles/{ params.user }/follow", null, locals.user.token)
+]
+
+del: async ([ :params, :locals ]) -> [
+	body: await api.del("profiles/{ params.user }/follow", locals.user.token)
+]
+
+(post, del)
