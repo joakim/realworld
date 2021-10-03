c8ab592876d7c9c6a7344c7d8f93516fbbd23935
Joakim <joakim@users.noreply.github.com>
2021-10-03 22:29:49 +0200

    Update _get_articles.kesh.lua

--- src/routes/profile/@[user]/_get_articles.kesh.lua
+++ src/routes/profile/@[user]/_get_articles.kesh.lua
@@ -1,25 +1,20 @@
 api: import '$lib/api.js'
 (page-size): '$lib/constants.js'
 
-get-articles: async ([ :query, params, locals ], type) -> {
+get-articles: async ([ :query, params, locals ], type) ->
 	p: +query.get('page') or 1
 
 	q: URLSearchParams [
 		['limit', page-size]
 		['offset', (p - 1) * page-size]
-		[type, encodeURIComponent params.user]
+		[type, encodeURIComponent(params.user)]
 	]
 
-	[ :articles, :articles-count ]: await api.get(
+	[:articles, :articles-count]: await api.get(
 		"articles?{ q }"
-		locals.user and locals.user.token
+		locals.user.token
 	)
 
-	[
-		body:
-			:articles,
-			pages: Math.ceil(articles-count / page-size)
-	]
-}
+	[ body: [ :articles, pages: Math.ceil(articles-count / page-size) ] ]
 
 (get-articles)
