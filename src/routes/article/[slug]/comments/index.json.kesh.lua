38cc7a85f4222a0792025c6d297b0f14b451a89e
Joakim <joakim@users.noreply.github.com>
2021-10-03 21:50:43 +0200

    Update and rename index.json.js to index.json.kesh.lua

---
+++ src/routes/article/[slug]/comments/index.json.kesh.lua
@@ -0,0 +1,44 @@
+api: import '$lib/api.js'
+
+get: async ([ :params, :locals ]) -> {
+	[:slug]: params
+	[:comments]: await api.get(
+		"articles/{ slug }/comments"
+		locals.user and locals.user.token
+	)
+	
+	[ body: comments ]
+}
+
+post: async ([ :params, body as form, :headers, :locals ]) -> {
+	if not locals.user { return [ status: 401 ] }
+	
+	[:slug]: params
+	body: form.get 'comment'
+	
+	[:comment]: await api.post(
+		"articles/{ slug }/comments"
+		[ comment: [ :body ] ]
+		locals.user.token
+	)
+	
+	-- for AJAX requests, return the newly created comment
+	if headers.accept = 'application/json' {
+		return [
+			status: 201 -- created
+			body: comment
+		]
+	}
+
+	-- for traditional (no-JS) form submissions, redirect
+	-- back to the article page
+	print("redirecting to /article/{ slug }")
+	
+	[
+		status: 303 -- see other
+		headers:
+			location: "/article/{ slug }"
+	]
+}
+
+(get, post)
