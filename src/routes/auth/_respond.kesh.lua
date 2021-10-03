ad4a1a0e6de2c95d7a8aae18c1561527e0fe03b1
Joakim <joakim@users.noreply.github.com>
2021-10-03 22:15:39 +0200

    Update _respond.kesh.lua

--- src/routes/auth/_respond.kesh.lua
+++ src/routes/auth/_respond.kesh.lua
@@ -1,10 +1,9 @@
-respond: (body) -> {
-	if body.errors { return [ status: 401, :body ] }
+respond: (body) ->
+	if body.errors? -> return [ status: 401, :body ]
 	
 	json: JSON.stringify body.user
 	value: Buffer.from(json).toString 'base64'
 	
 	[ headers: 'set-cookie': "jwt={ value }; Path=/; HttpOnly", :body ]
-}
 
 (respond)
