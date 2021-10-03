1b3b823eb5d4978fb864bdf1280826ba8f3bf08b
Joakim <joakim@users.noreply.github.com>
2021-10-03 22:16:47 +0200

    Update register.kesh.lua

--- src/routes/auth/register.kesh.lua
+++ src/routes/auth/register.kesh.lua
@@ -1,13 +1,12 @@
 api: import '$lib/api.js'
 (respond): import './_respond'
 
-post: async (request) -> {
+post: async (request) ->
 	user: request.body
 	
 	-- TODO individual properties
 	body: await api.post('users', [ :user ])
 	
 	respond body
-}
 
 (post)
