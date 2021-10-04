03abf8c52072c0d5969adcb66ca67fa57bc4d6d3
Joakim <joakim@users.noreply.github.com>
2021-10-04 22:53:55 +0200

    Update index.svelte

--- src/lib/ArticleList/index.svelte
+++ src/lib/ArticleList/index.svelte
@@ -1,16 +1,18 @@
 <script>
-	import { session } from '$app/stores';
-	import ArticlePreview from './ArticlePreview.svelte';
-
-	export let articles;
+	[:session]: import '$app/stores'
+	(default as ArticlePreview): import './ArticlePreview.svelte'
+	
+	let articles
+	
+	(articles)
 </script>
 
-{#if articles.length === 0}
+{#if articles.length = 0}
 	<div class="article-preview">No articles are here... yet.</div>
 {:else}
 	<div>
 		{#each articles as article (article.slug)}
-			<ArticlePreview {article} user={$session.user} />
+			<ArticlePreview { article } user={ $session.user } />
 		{/each}
 	</div>
 {/if}
