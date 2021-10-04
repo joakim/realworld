15be7cc415ab2096e8552c512a253ebe40372c21
Joakim <joakim@users.noreply.github.com>
2021-10-04 22:30:57 +0200

    Update and rename actions.js to actions.kesh.lua

---
+++ src/lib/actions.kesh.lua
@@ -0,0 +1,29 @@
+no-op: () -> ()
+
+ajax: (
+	node
+	[
+		:on-submit ? no-op
+		:on-response ? no-op
+	] ? []
+) ->
+	handler: async (event) *->
+		event.preventDefault()
+		body: FormData(node) if node.method = 'post' or node.method = 'put' else null
+		
+		on-submit body
+
+		response: await fetch(node.action, [
+			method: node.method
+			body
+			headers:
+				accept: 'application/json'
+		])
+		
+		on-response response
+	
+	node.addEventListener('submit', handler)
+	
+	[ destroy: () *-> node.removeEventListener('submit', handler) ]
+
+(ajax)
