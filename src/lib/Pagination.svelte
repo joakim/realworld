18f29c72cbc6dd6ed82775aab7cdf30ab29177cc
Joakim <joakim@users.noreply.github.com>
2021-10-04 22:38:50 +0200

    Update Pagination.svelte

--- src/lib/Pagination.svelte
+++ src/lib/Pagination.svelte
@@ -1,23 +1,20 @@
 <script>
-	export let pages;
-	export let p;
-	export let href;
-
-	let range;
-
-	$: {
-		range = [];
-		for (let i = 1; i <= pages; ++i) {
-			range.push(i);
-		}
-	}
+	let pages
+	let p
+	let href
+	
+	let range
+	
+	$ -> set range: array.from [1..pages]
+	
+	(pages, p, href)
 </script>
 
 {#if pages > 1}
 	<nav>
 		<ul class="pagination">
 			{#each range as n}
-				<li class="page-item" class:active={n == p}><a class="page-link" href={href(n)}>{n}</a></li>
+				<li class="page-item" class:active={ n = p }><a class="page-link" href={ href n }>{ n }</a></li>
 			{/each}
 		</ul>
 	</nav>
