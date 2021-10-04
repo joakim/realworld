f3ac5cba8d5dca4c8e6907735c9cd5ac78be5c24
Joakim <joakim@users.noreply.github.com>
2021-10-04 22:32:07 +0200

    Update Counter.svelte

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,13 +1,11 @@
 <script>
-	export let count = 0;
-
-	const increment = () => {
-		count += 1;
-	};
+	let count: 0
+	
+	increment: () *-> set count: count + 1
 </script>
 
-<button on:click={increment}>
-	Clicks: {count}
+<button on:click={ increment }>
+	Clicks: { count }
 </button>
 
 <style>
