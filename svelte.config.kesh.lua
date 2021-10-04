cd7b87cb50be33f4930726791856ecddd10b90cc
Joakim <joakim@users.noreply.github.com>
2021-10-04 23:01:57 +0200

    Update svelte.config.kesh.lua

--- svelte.config.kesh.lua
+++ svelte.config.kesh.lua
@@ -1,10 +1,8 @@
 (default as netlify): import '@sveltejs/adapter-netlify'
 (default as vercel): import '@sveltejs/adapter-vercel'
 
-default: [
+(default: [
 	kit:
 		adapter: vercel() if process.env.VERCEL else netlify()
 		target: '#svelte'
-]
-
-(default)
+])
