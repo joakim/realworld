c569d5b127bf8594dd28efd31c8bbcce31d28937
Joakim <joakim@users.noreply.github.com>
2021-10-04 22:34:51 +0200

    Update Nav.svelte

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { page, session } from '$app/stores';
+	[:page, :session]: import '$app/stores'
 </script>
 
 <nav class="navbar navbar-light">
@@ -7,12 +7,12 @@
 		<a rel="prefetch" class="navbar-brand" href="/">conduit</a>
 		<ul class="nav navbar-nav pull-xs-right">
 			<li class="nav-item">
-				<a rel="prefetch" class="nav-link" class:active={$page.path === '/'} href="/">Home</a>
+				<a rel="prefetch" class="nav-link" class:active={ $page.path = '/' } href="/">Home</a>
 			</li>
 
 			{#if $session.user}
 				<li class="nav-item">
-					<a rel="prefetch" href="/editor" class="nav-link" class:active={$page.path === '/editor'}>
+					<a rel="prefetch" href="/editor" class="nav-link" class:active={ $page.path = '/editor' }>
 						<i class="ion-compose" />&nbsp;New Post
 					</a>
 				</li>
@@ -22,20 +22,20 @@
 						rel="prefetch"
 						href="/settings"
 						class="nav-link"
-						class:active={$page.path === '/settings'}>
+						class:active={ $page.path = '/settings' }>
 						<i class="ion-gear-a" />&nbsp;Settings
 					</a>
 				</li>
 
 				<li class="nav-item">
-					<a rel="prefetch" href="/profile/@{$session.user.username}" class="nav-link">
-						<!-- <img src={$user.image} class="user-pic" alt={$user.username}> -->
-						{$session.user.username}
+					<a rel="prefetch" href="/profile/@{ $session.user.username }" class="nav-link">
+						<!-- <img src={ $user.image } class="user-pic" alt={ $user.username }> -->
+						{ $session.user.username }
 					</a>
 				</li>
 			{:else}
 				<li class="nav-item">
-					<a rel="prefetch" href="/login" class="nav-link" class:active={$page.path === '/login'}>
+					<a rel="prefetch" href="/login" class="nav-link" class:active={ $page.path = '/login' }>
 						Sign in
 					</a>
 				</li>
@@ -45,7 +45,7 @@
 						rel="prefetch"
 						href="/register"
 						class="nav-link"
-						class:active={$page.path === '/register'}>
+						class:active={ $page.path = '/register' }>
 						Sign up
 					</a>
 				</li>
