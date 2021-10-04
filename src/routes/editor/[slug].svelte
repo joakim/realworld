02d1895acd9ab6e8cbde810f6a5dafe7dc35aded
Joakim <joakim@users.noreply.github.com>
2021-10-05 01:55:28 +0200

    Update [slug].svelte

--- src/routes/editor/[slug].svelte
+++ src/routes/editor/[slug].svelte
@@ -2,7 +2,8 @@
 	api: import '$lib/api.js'
 	
 	load: async ([ :page, :session ]) ->
-		if not session.user? -> return [ redirect: '/login', status: 302 ]
+		if not session.user?
+			return [ redirect: '/login', status: 302 ]
 		
 		[:slug]: page.params
 		[:article]: await api.get("articles/{ slug }", null)
