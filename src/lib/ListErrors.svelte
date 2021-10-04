e2415bb7ec6b61832fa537637756c0cbaa77d27b
Joakim <joakim@users.noreply.github.com>
2021-10-04 22:33:07 +0200

    Update ListErrors.svelte

--- src/lib/ListErrors.svelte
+++ src/lib/ListErrors.svelte
@@ -1,11 +1,12 @@
 <script>
-	export let errors;
+	let errors
+	(errors)
 </script>
 
 {#if errors}
 	<ul class="error-messages">
-		{#each Object.keys(errors) as key}
-			<li>{key} {errors[key]}</li>
+		{#each #collection.keys(errors) as key}
+			<li>{ key } { errors.{ key } }</li>
 		{/each}
 	</ul>
 {/if}
