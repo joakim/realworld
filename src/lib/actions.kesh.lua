b89bdff4d932c079bca9ffeb6f90954420bd7271
Joakim <joakim@users.noreply.github.com>
2021-10-05 13:06:33 +0200

    Update actions.kesh.lua

--- src/lib/actions.kesh.lua
+++ src/lib/actions.kesh.lua
@@ -1,21 +1,15 @@
 no-op: () -> ()
 
-ajax: (
-	node
-	[
-		:on-submit ? no-op
-		:on-response ? no-op
-	] ? []
-) ->
+ajax: (node, [on-submit ? no-op, on-response ? no-op] ? []) ->
 	handler: async (event) *->
 		event.preventDefault()
 		body: FormData(node) if node.method = 'post' or node.method = 'put' else null
 		
 		on-submit body
-
+		
 		response: await fetch(node.action, [
 			method: node.method
-			body
+			:body
 			headers:
 				accept: 'application/json'
 		])
