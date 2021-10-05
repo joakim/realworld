f38f11bc29ded93903497f91f771c99fd14f5288
Joakim <joakim@users.noreply.github.com>
2021-10-05 17:05:01 +0200

    Update index.json.kesh.lua

--- src/routes/profile/@[user]/index.json.kesh.lua
+++ src/routes/profile/@[user]/index.json.kesh.lua
@@ -1,7 +1,8 @@
 api: import '$lib/api.js'
 
-get: async ([ :params, :locals ]) ->
-	[:profile]: await api.get("profiles/{ params.user }", locals.user.token)
-	[ body: profile ]
+get: async [params, locals] ->
+	[profile]: await api.get("profiles/{ params.user }", locals.user.token)
+	
+	(body: profile)
 
 (get)
