ea726a7d89a03a66d5577cc07c668f80f3b61836
Joakim <joakim@users.noreply.github.com>
2021-10-05 17:04:24 +0200

    Update follow.kesh.lua

--- src/routes/profile/@[user]/follow.kesh.lua
+++ src/routes/profile/@[user]/follow.kesh.lua
@@ -1,10 +1,10 @@
 api: '$lib/api.js'
 
-post: async ([ :params, :locals ]) -> [
+post: async [params, locals] -> [
 	body: await api.post("profiles/{ params.user }/follow", null, locals.user.token)
 ]
 
-del: async ([ :params, :locals ]) -> [
+del: async [params, locals] -> [
 	body: await api.del("profiles/{ params.user }/follow", locals.user.token)
 ]
 
