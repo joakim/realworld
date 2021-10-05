5b99d8eeff714e9f64826d9db5e1486e4c3fa200
Joakim <joakim@users.noreply.github.com>
2021-10-05 12:19:27 +0200

    Update [id].json.kesh.lua

--- src/routes/article/[slug]/comments/[id].json.kesh.lua
+++ src/routes/article/[slug]/comments/[id].json.kesh.lua
@@ -1,13 +1,13 @@
 api: import '$lib/api.js'
 
-del: async ([ .params, .locals ]) *->
+del: async ([ params, locals ]) *->
 	if not locals.user?
 		return [ status: 401 ]
 	
-	[.slug, .id]: params
-	[.status, .error]: await api.del("articles/{ slug }/comments/{ id }", locals.user.token)
+	[slug, id]: params
+	[status, error]: await api.del("articles/{ slug }/comments/{ id }", locals.user.token)
 	
 	if error?
-		return [ :status, body: [ :error ] ]
+		return [ status, body: [ error ] ]
 
 (del)
