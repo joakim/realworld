9adb48199bbea4ca2ac1849e03208248f2933fce
Joakim <joakim@users.noreply.github.com>
2021-10-05 12:58:45 +0200

    Update ArticlePreview.svelte

--- src/lib/ArticleList/ArticlePreview.svelte
+++ src/lib/ArticleList/ArticlePreview.svelte
@@ -13,7 +13,7 @@
 			set article.favoritesCount: + 1
 			set article.favorited: true
 		
-		[ :article ]: await ->
+		[article]: await ->
 		 	if article.favorited?
 				api.post("articles/{ article.slug }/favorite", null, user.token)
 			else
@@ -24,35 +24,35 @@
 
 <div class="article-preview">
 	<div class="article-meta">
-		<a href="/profile/@{article.author.username}">
-			<img src={article.author.image} alt={article.author.username} />
+		<a href="/profile/@{ article.author.username }">
+			<img src={ article.author.image } alt={ article.author.username } />
 		</a>
 
 		<div class="info">
-			<a class="author" href="/profile/@{article.author.username}"> {article.author.username} </a>
-			<span class="date"> {new Date(article.createdAt).toDateString()} </span>
+			<a class="author" href="/profile/@{ article.author.username }"> { article.author.username } </a>
+			<span class="date"> { Date(article.createdAt).toDateString() } </span>
 		</div>
 
 		{#if user}
 			<div class="pull-xs-right">
 				<button
-					class="btn btn-sm {article.favorited ? 'btn-primary' : 'btn-outline-primary'}"
-					on:click={toggle_favorite}
+					class="btn btn-sm { 'btn-primary' if article.favorited else 'btn-outline-primary' }"
+					on:click={ toggle-favorite }
 				>
 					<i class="ion-heart" />
-					{article.favoritesCount}
+					{ article.favoritesCount }
 				</button>
 			</div>
 		{/if}
 	</div>
 
-	<a href="/article/{article.slug}" rel="prefetch" class="preview-link">
-		<h1>{article.title}</h1>
-		<p>{article.description}</p>
+	<a href="/article/{ article.slug }" rel="prefetch" class="preview-link">
+		<h1>{ article.title }</h1>
+		<p>{ article.description }</p>
 		<span>Read more...</span>
 		<ul class="tag-list">
 			{#each article.tagList as tag}
-				<li class="tag-default tag-pill tag-outline"><a href="/?tag={tag}">{tag}</a></li>
+				<li class="tag-default tag-pill tag-outline"><a href="/?tag={ tag }">{ tag }</a></li>
 			{/each}
 		</ul>
 	</a>
