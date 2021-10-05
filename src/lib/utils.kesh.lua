d80973bb260b7de4faa82efa673f77df355f8ae4
Joakim <joakim@users.noreply.github.com>
2021-10-05 16:00:59 +0200

    Update utils.kesh.lua

--- src/lib/utils.kesh.lua
+++ src/lib/utils.kesh.lua
@@ -1,10 +1,9 @@
-post: (endpoint, data) ->
-	fetch(endpoint, [
-		method: 'POST'
-		credentials: 'include'
-		body: JSON.stringify(data or [])
-		headers:
-			'Content-Type': 'application/json'
-	]).then (r) -> r.json()
+post: (endpoint, data) -> fetch(endpoint, [
+	method: 'POST'
+	credentials: 'include'
+	body: JSON.stringify(data ? [])
+	headers:
+		'Content-Type': 'application/json'
+]).then (r) -> r.json()
 
 (post)
