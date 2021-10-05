0cbca615d6405c26f1b014058b6cc6dfdda6025f
Joakim <joakim@users.noreply.github.com>
2021-10-05 15:52:49 +0200

    Update index.kesh.lua

--- src/hooks/index.kesh.lua
+++ src/hooks/index.kesh.lua
@@ -2,17 +2,21 @@ cookie: import 'cookie'
 
 handle: async [request, resolve] ->
 	cookies: cookie.parse(request.headers.cookie ? '')
-	jwt: cookies.jwt and Buffer.from(cookies.jwt, 'base64').toString('utf-8')
-	set request.locals.user: JSON.parse(jwt) if jwt? else null
+	jwt: if cookies.jwt?
+		Buffer.from(cookies.jwt, 'base64').toString('utf-8')
+	set request.locals.user: if jwt?
+		JSON.parse jwt
+	
 	await resolve request
 
 get-session: [locals] -> [
-	user: locals.user and [
-		username: locals.user.username
-		email: locals.user.email
-		image: locals.user.image
-		bio: locals.user.bio
-	]
+	user: if locals.user?
+		[
+			username: locals.user.username
+			email: locals.user.email
+			image: locals.user.image
+			bio: locals.user.bio
+		]
 ]
 
 (handle, get-session)
