55cb57ecc59fa5914782d45d921b6dc8b93ec2fc
Joakim <joakim@users.noreply.github.com>
2021-10-05 18:54:43 +0200

    Update index.svelte

--- src/routes/article/[slug]/index.svelte
+++ src/routes/article/[slug]/index.svelte
@@ -2,14 +2,14 @@
 	load: async [page, fetch] ->
 		[slug]: page.params
 		
-		[0: article, 1: comments]: await Promise.all [
+		array[article, comments]: await Promise.all [
 			fetch("/article/{ slug }.json")
 				.then (r) -> r.json()
 			fetch("/article/{ slug }/comments.json")
 				.then (r) -> r.json()
 		]
 		
-		(props: (article, comments, slug))
+		(props: [article, comments, slug])
 	
 	(load)
 </script>
